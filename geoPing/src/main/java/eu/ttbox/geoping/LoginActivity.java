279e6be29b7131c6eb147c0f026aa31cabd165c7
jmorille <jmorille@gmail.com>
2013-11-06 21:51:49 +0100

    counter

--- geoPing/src/main/java/eu/ttbox/geoping/LoginActivity.java
+++ geoPing/src/main/java/eu/ttbox/geoping/LoginActivity.java
@@ -4,8 +4,16 @@ package eu.ttbox.geoping;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.widget.TextView;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 import eu.ttbox.geoping.core.AppConstants;
 import eu.ttbox.geoping.ui.lock.prefs.CommandsPrefsHelper;
@@ -15,7 +23,6 @@ public class LoginActivity extends ActionBarActivity { //
 
     private static final String TAG = "LoginActivity";
 
-    private SharedPreferences loginPrefs;
 
     private static final String PREF_RETRY_COUNT = "retry_count";
 
@@ -24,6 +31,12 @@ public class LoginActivity extends ActionBarActivity { //
 
     private static final String PREF_LOGIN_FAILED_DATE = "login_failed_date";
     private static final String PREF_LOGIN_FAILED_COUNT = "login_failed_count";
+
+    // Service
+    private SharedPreferences loginPrefs;
+    private ScheduledExecutorService scheduler;
+    // Binding
+    private TextView loginText;
 
     // ===========================================================
     // Constructors
@@ -37,11 +50,83 @@ public class LoginActivity extends ActionBarActivity { //
             // Open Login
             int previousRetryCount = loginPrefs.getInt(PREF_RETRY_COUNT, 0);
             CommandsPrefsHelper.startActivityPatternCompare(this, previousRetryCount);
+            // Init Binding
+            initBinding();
         } else {
             startMainActivity();
         }
     }
 
+    private void initBinding() {
+        setContentView(R.layout.login_activity);
+        loginText = (TextView) findViewById(R.id.login_status_textView);
+    }
+
+
+    // ===========================================================
+    // Life Cycle
+    // ===========================================================
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (scheduler != null) {
+            scheduler.shutdown();
+            scheduler = null;
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (scheduler != null) {
+            scheduler.shutdownNow();
+            scheduler = null;
+        }
+    }
+
+    // ===========================================================
+    // Scheduler
+    // ===========================================================
+
+    private static final String HANDLER_MESSAGE = "MSG";
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            Bundle bundle = msg.getData();
+            String displayText = bundle.getString(HANDLER_MESSAGE);
+            loginText.setText(displayText);
+        }
+    };
+
+    private void schedulerTimer() {
+        if (scheduler == null) {
+            scheduler = Executors.newScheduledThreadPool(1);
+        }
+        final Runnable beeper = new Runnable() {
+            private int counterInS = 30;
+
+            public void run() {
+                // Dec Counter
+                counterInS = counterInS - 1;
+                // Define Text
+                String displayText = "Try again in " + counterInS + " seconds";
+                Log.d(TAG, displayText);
+                // Send Message
+                Message myMessage = handler.obtainMessage();
+                myMessage.getData().putString(HANDLER_MESSAGE, displayText);
+                handler.sendMessage(myMessage);
+            }
+        };
+        // Schedule Tasks
+        ScheduledFuture<?> beeperHandle =  scheduler.scheduleAtFixedRate(beeper, 0, 1, TimeUnit.SECONDS);
+    }
 
     // ===========================================================
     // Handle Intent
@@ -49,7 +134,7 @@ public class LoginActivity extends ActionBarActivity { //
 
 
     private void incrementKey(SharedPreferences.Editor prefEditor, String pkey, int incCount) {
-       if (incCount != 0) {
+        if (incCount != 0) {
             int previousCount = loginPrefs.getInt(pkey, 0);
             int incVal = incCount + previousCount;
             prefEditor.putInt(pkey, incVal);
@@ -64,7 +149,6 @@ public class LoginActivity extends ActionBarActivity { //
             case CommandsPrefsHelper.REQ_ENTER_PATTERN:
             case CommandsPrefsHelper.REQ_VERIFY_CAPTCHA: {
                 int msgId = 0;
-
                 int retryCount = data.getIntExtra(LockPatternActivity.EXTRA_RETRY_COUNT, 0);
                 SharedPreferences.Editor prefEditor = loginPrefs.edit();
                 long now = System.currentTimeMillis();
@@ -99,7 +183,8 @@ public class LoginActivity extends ActionBarActivity { //
                             prefEditor.putInt(PREF_RETRY_COUNT, 0);
                             // Increment Failed Count
                             incrementKey(prefEditor, PREF_LOGIN_FAILED_COUNT, 1);
-                            // finish();
+                            // Define Timer
+                            schedulerTimer();
                         }
                         break;
                         case LockPatternActivity.RESULT_FORGOT_PATTERN: {
