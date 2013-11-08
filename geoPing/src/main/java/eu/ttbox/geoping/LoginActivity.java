64061e651cf5af6f311526640058e766a612c7d6
jmorille <jmorille@gmail.com>
2013-11-08 22:49:29 +0100

    lock

--- geoPing/src/main/java/eu/ttbox/geoping/LoginActivity.java
+++ geoPing/src/main/java/eu/ttbox/geoping/LoginActivity.java
@@ -19,11 +19,17 @@ public class LoginActivity extends ActionBarActivity { //
 
     private static final String TAG = "LoginActivity";
 
+    private final long LOCK_BASE_TIME_IN_MS = 30 * 1000;
+
     // Service
     private SharedPreferences loginPrefs;
     private CountDownTimer countDownTimer;
+
     // Binding
     private TextView mSecurityMessageDisplay;
+
+    // Config instance
+    private String user = "Local_";
 
     // ===========================================================
     // Constructors
@@ -34,8 +40,6 @@ public class LoginActivity extends ActionBarActivity { //
         super.onCreate(savedInstanceState);
         if (CommandsPrefsHelper.isPassword(this)) {
             loginPrefs = getSharedPreferences(AppConstants.PREFS_FILE_LOGIN, MODE_PRIVATE);
-            // Open Login
-            openPromptPassword();
             // Init Binding
             initBinding();
         } else {
@@ -45,7 +49,7 @@ public class LoginActivity extends ActionBarActivity { //
 
     private void initBinding() {
         setContentView(R.layout.login_activity);
-        mSecurityMessageDisplay =(TextView)findViewById(R.id.keyguard_message_area);
+        mSecurityMessageDisplay = (TextView) findViewById(R.id.keyguard_message_area);
     }
 
 
@@ -56,59 +60,82 @@ public class LoginActivity extends ActionBarActivity { //
     @Override
     public void onPause() {
         super.onPause();
-//        if (countDownTimer != null) {
-//            countDownTimer.cancel();
-//        }
+        if (countDownTimer != null) {
+            Log.d(TAG, "### onPause : countDownTimer.cancel()");
+            countDownTimer.cancel();
+            countDownTimer = null;
+        }
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        if (!lockScreen()) {
+            Log.d(TAG, "### onResume : openPromptPassword");
+            openPromptPassword();
+        }
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
         if (countDownTimer != null) {
+            Log.d(TAG, "### onDestroy : countDownTimer.cancel()");
             countDownTimer.cancel();
+            countDownTimer = null;
         }
     }
 
     // ===========================================================
     // Action
     // ===========================================================
-    private void openPromptPassword(){
-        int previousRetryCount = readPrefInt(R.string.pkey_login_retry_count, 0);
-        CommandsPrefsHelper.startActivityPatternCompare(this, previousRetryCount);
+
+    private long getExpectedEnlapseLockTime(int failedCount) {
+        // exponential Algo
+        long failFactor = Math.round(Math.exp(Math.max(0, failedCount - 1)));
+//        failFactor = 0;
+        long enlapseExpectedLockTime = failFactor * LOCK_BASE_TIME_IN_MS;
+        Log.d(TAG, "### Fail count " + failedCount + " ==> Factor : " + failFactor + " ==> " + enlapseExpectedLockTime + "ms");
+        return enlapseExpectedLockTime;
     }
 
-    private final  long LOCK_BASE_TIME_IN_MS = 30* 1000;
 
-    private void long isLockApp() {
-        long lockTime = 0;
+    private long getRestLockTimeInMs() {
         int failedCount = readPrefInt(R.string.pkey_login_failed_count, 0);
-        if (failedCount>0) {
+        return getRestLockTimeInMs(failedCount);
+    }
+
+    private long getRestLockTimeInMs(int failedCount) {
+        long restLockTimeInMs = 0;
+        if (failedCount > 0) {
             long now = System.currentTimeMillis();
             long failedDate = readPrefLong(R.string.pkey_login_failed_date, 0);
+            // Lock Time
+            long enlapseExpectedLockTime = getExpectedEnlapseLockTime(failedCount);
             // Compute Time
             long enlapseRealTime = now - failedDate;
-            long enlapseExpectedLockTime = failedCount * LOCK_BASE_TIME_IN_MS; // TODO exponential Algo
-            long enlapseExpectedTime = now - enlapseExpectedLockTime; // TODO exponential Algo
-            long restTimeInMs = enlapseExpectedLockTime - enlapseRealTime;
+            if (enlapseRealTime >= enlapseExpectedLockTime) {
+                // The lock Time is past
+                return 0;
+            } else if (enlapseRealTime < 0) {
+                // The system date as change, hack in progress ?
+                Log.e(TAG, "The system date as change, hack in progress ?");
+                enlapseExpectedLockTime = getExpectedEnlapseLockTime(failedCount * 4);
+            } else {
+                // Compute Resting time
+                restLockTimeInMs = enlapseExpectedLockTime - enlapseRealTime;
+            }
         }
-        return lockTime;
+        return restLockTimeInMs;
     }
 
-    private void lockScreen() {
-        // Read Param
-       long failedDate = readPrefLong(R.string.pkey_login_failed_date, 0);
-       int failedCount = readPrefInt(R.string.pkey_login_failed_count, 1);
-        // Compute Lock Time
-        long now = System.currentTimeMillis();
-//        long lockPass =
-        // Apply Lock
-  //      startCountDownInMinutes(3);
-        startCountDownInSeconds(30*failedCount);
+    private boolean lockScreen() {
+        long restLockTimeInMs = getRestLockTimeInMs();
+        boolean isLockNeed = restLockTimeInMs > 0;
+        if (isLockNeed) {
+            startCountDownInSeconds(restLockTimeInMs);
+        }
+        return isLockNeed;
     }
 
 
@@ -127,58 +154,45 @@ public class LoginActivity extends ActionBarActivity { //
 
     };
 
-    /**
-     * ./platform_frameworks_base/policy/src/com/android/internal/policy/impl/keyguard/KeyguardPatternView.java
-     * @param minutes
-     */
-    private void startCountDownInMinutes(int minutes) {
-        int countMin = minutes;
-//        if (minutes<)
-
-        countDownTimer = new CountDownTimer(countMin * 60 * 1000, 60 *1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                final int secondsRemaining = (int) (millisUntilFinished / 1000);
-                String msgDisplay = getString( R.string.kg_too_many_failed_attempts_countdown, secondsRemaining);
-                mSecurityMessageDisplay.setText(msgDisplay);
-
-                int counterInMin = (int)( millisUntilFinished / (60 * 1000));
-
-                String displayText = "Try again in " + counterInMin + " minutes";
-                // Display Text
-                Message msg  = handler.obtainMessage();
-                msg.getData().putString(HANDLER_DISPLAY_TEXT, displayText);
-                handler.dispatchMessage(msg);
-            }
-
-            @Override
-            public void onFinish() {
-                String displayText = "Try again in 1 minutes";
-                startCountDownInSeconds(59);
-            }
-        };
-
-        countDownTimer.start();
+    private void setDisplayText(long millisUntilFinished) {
+        final long secondsRemaining =  Math.round(millisUntilFinished / 1000);
+        String msgDisplay = getString(R.string.kg_too_many_failed_attempts_countdown, secondsRemaining);
+        if (secondsRemaining >= 60l) {
+            long s = secondsRemaining % 60;
+            long min = (secondsRemaining / (60)) % 60;
+            long hour = (secondsRemaining / (60 * 60)) % 24;
+            Log.d(TAG, "### Time to finish : " + hour + "h " + min + "min " + s + "s");
+        } else {
+            Log.d(TAG, "### Time to finish : " + secondsRemaining + "s");
+        }
+        mSecurityMessageDisplay.setText(msgDisplay);
     }
 
-    private void startCountDownInSeconds(int seconds) {
-        countDownTimer = new CountDownTimer(seconds * 1000, 1000) {
+    private synchronized void startCountDownInSeconds(long lockTimeInMs) {
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+            Log.w(TAG, "### Cancel previous CountDown");
+        }
+        countDownTimer = new CountDownTimer(lockTimeInMs, 1000) {
+
             @Override
             public void onTick(long millisUntilFinished) {
-                final int secondsRemaining = (int) (millisUntilFinished / 1000);
-                String msgDisplay = getString( R.string.kg_too_many_failed_attempts_countdown, secondsRemaining);
-                mSecurityMessageDisplay.setText(msgDisplay);
+                setDisplayText(millisUntilFinished);
             }
 
             @Override
             public void onFinish() {
                 String displayText = "";
                 mSecurityMessageDisplay.setText(displayText);
-                //
+                //    if (countDownTimer != null) {
                 countDownTimer = null;
+                Log.d(TAG, "### CountDown onFinish : openPromptPassword");
                 openPromptPassword();
+                //     }
             }
         };
+        Log.i(TAG, "### Start CountDown for : " + lockTimeInMs + " ms");
+        setDisplayText(lockTimeInMs);
         countDownTimer.start();
     }
 
@@ -192,29 +206,34 @@ public class LoginActivity extends ActionBarActivity { //
         if (incCount != 0) {
             int previousCount = readPrefInt(pkeyId, 0);
             int incVal = incCount + previousCount;
-            writePrefInt( prefEditor, pkeyId, incVal);
+            writePrefInt(prefEditor, pkeyId, incVal);
             Log.d(TAG, "### Increment " + getString(pkeyId) + " : " + previousCount + " ===> " + incVal);
         }
     }
 
     private void writePrefLong(SharedPreferences.Editor prefEditor, int pkeyId, long val) {
-        String key = getString(pkeyId);
+        String key = getPrefKey(pkeyId);
         prefEditor.putLong(key, val);
     }
 
     private long readPrefLong(int pkeyId, long defaultVal) {
-        String key = getString(pkeyId);
+        String key = getPrefKey(pkeyId);
         return loginPrefs.getLong(key, defaultVal);
     }
 
     private void writePrefInt(SharedPreferences.Editor prefEditor, int pkeyId, int val) {
-        String key = getString(pkeyId);
+        String key = getPrefKey(pkeyId);
         prefEditor.putInt(key, val);
     }
 
     private int readPrefInt(int pkeyId, int defaultVal) {
-        String key = getString(pkeyId);
+        String key = getPrefKey(pkeyId);
         return loginPrefs.getInt(key, defaultVal);
+    }
+
+    private String getPrefKey(int pkeyId) {
+        String key = user + getString(pkeyId);
+        return key;
     }
 
 
@@ -223,35 +242,32 @@ public class LoginActivity extends ActionBarActivity { //
     // ===========================================================
 
 
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        boolean needToLock = false;
         switch (requestCode) {
             case CommandsPrefsHelper.REQ_ENTER_PATTERN:
             case CommandsPrefsHelper.REQ_VERIFY_CAPTCHA: {
-                int msgId = 0;
                 int retryCount = data.getIntExtra(LockPatternActivity.EXTRA_RETRY_COUNT, 0);
                 SharedPreferences.Editor prefEditor = loginPrefs.edit();
                 long now = System.currentTimeMillis();
                 try {
                     switch (resultCode) {
                         case RESULT_OK: {
-                            // The user passed
-                            msgId = android.R.string.ok;
-                            startMainActivity();
                             // Mark Success
-                            writePrefLong(prefEditor, R.string.pkey_login_success_date  , now);
+                            writePrefLong(prefEditor, R.string.pkey_login_success_date, now);
                             incrementKey(prefEditor, R.string.pkey_login_success_count, 1);
                             // Reset retry
                             writePrefInt(prefEditor, R.string.pkey_login_retry_count, 0);
                             // Reset Failed
                             writePrefLong(prefEditor, R.string.pkey_login_failed_date, Long.MIN_VALUE);
                             writePrefInt(prefEditor, R.string.pkey_login_failed_count, 0);
+                            // The user passed
+                            startMainActivity();
                         }
                         break;
                         case RESULT_CANCELED: {
                             // The user cancelled the task
-                            msgId = android.R.string.cancel;
                             incrementKey(prefEditor, R.string.pkey_login_retry_count, retryCount);
                             finish();
                         }
@@ -261,11 +277,11 @@ public class LoginActivity extends ActionBarActivity { //
                             // Mark Failed
                             writePrefLong(prefEditor, R.string.pkey_login_failed_date, now);
                             // Reset retry
-                            writePrefInt(prefEditor,  R.string.pkey_login_retry_count, 0);
+                            writePrefInt(prefEditor, R.string.pkey_login_retry_count, 0);
                             // Increment Failed Count
                             incrementKey(prefEditor, R.string.pkey_login_failed_count, 1);
                             // Define Timer
-                            lockScreen();
+                            needToLock = true;
                         }
                         break;
                         case LockPatternActivity.RESULT_FORGOT_PATTERN: {
@@ -277,11 +293,14 @@ public class LoginActivity extends ActionBarActivity { //
                             finish();
                             return;
                     }
-                } finally { 
+                } finally {
                     prefEditor.commit();
-
                 }
-
+                // lock
+                if (needToLock) {
+                    Log.d(TAG, "### onActivityResult : lockScreen");
+                    lockScreen();
+                }
             }
         }
     }
@@ -290,10 +309,16 @@ public class LoginActivity extends ActionBarActivity { //
     // Handle Intent
     // ===========================================================
 
+    private void openPromptPassword() {
+        int previousRetryCount = readPrefInt(R.string.pkey_login_retry_count, 0);
+        Log.d(TAG, "### Open Prompt Password : " + previousRetryCount + " retry");
+        CommandsPrefsHelper.startActivityPatternCompare(this, previousRetryCount);
+    }
+
     public void startMainActivity() {
+        finish();
         Intent mainActivity = new Intent(this, MainActivity.class);
         startActivity(mainActivity);
-        finish();
     }
 
 
