d5495acdff18cfeccecb11f14e048d280a526fd5
jmorille <jmorille@gmail.com>
2014-04-05 17:32:35 +0200

    geofence bad refacto

--- geoPing/src/main/java/eu/ttbox/geoping/ui/geofence/GeofenceEditFragment.java
+++ geoPing/src/main/java/eu/ttbox/geoping/ui/geofence/GeofenceEditFragment.java
@@ -73,7 +73,7 @@ public class GeofenceEditFragment extends Fragment {
         setHasOptionsMenu(true);
 
         // Bindings
-        this.nameEditText = (EditText) v.findViewById(R.id.emergencyPasswordEdit);
+        this.nameEditText = (EditText) v.findViewById(R.id.geofenceEditName);
         this.transitionEnterCheckBox = (CompoundButton) v.findViewById(R.id.geofence_transition_enter_checkBox);
         this.transitionExitCheckBox = (CompoundButton) v.findViewById(R.id.geofence_transition_exit_checkBox);
         //
